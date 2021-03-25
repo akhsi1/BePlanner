eca61b2a7fea00384c999b89c06c0fb8e6683104
kang <akhsi1@student.monash.edu>
2021-03-25 08:40:13 +0000

    [VP]
    X-VP-Version: 4.0-beta2
    X-VP-Environment: default

--- wp-content/themes/astra/header.php
+++ wp-content/themes/astra/header.php
@@ -41,6 +41,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	);
 	?>
 >
+
 	<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( astra_default_strings( 'string-header-skip-link', false ) ); ?></a>
 	<?php 
 	astra_header_before(); 
