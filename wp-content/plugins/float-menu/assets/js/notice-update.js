7aa739c401428cff465158eec1a35e10b040f716
kang <akhsi1@student.monash.edu>
2021-03-25 03:28:47 +0000

    [VP] Deleted plugin 'Float Menu Lite'
    VP-Action: plugin/delete/float-menu/float-menu.php
    VP-Plugin-Name: Float Menu Lite
    
    X-VP-Version: 4.0-beta2
    X-VP-Environment: default

--- wp-content/plugins/float-menu/assets/js/notice-update.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).on( 'click', '.wow-plugin-notice .notice-dismiss', function() {
-		jQuery.ajax({
-        url: ajaxurl,
-        data: {
-            action: 'float_menu_notice_action'
-        }
-    })
-})
