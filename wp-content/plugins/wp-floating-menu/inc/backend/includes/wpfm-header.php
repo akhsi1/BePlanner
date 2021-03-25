7ddeb2b5b73e423539f22f7e5f27d93e0ffde4dc
kang <akhsi1@student.monash.edu>
2021-03-25 04:27:30 +0000

    [VP] Deleted plugin 'WP Floating Menu'
    VP-Action: plugin/delete/wp-floating-menu/wp-floating-menu.php
    VP-Plugin-Name: WP Floating Menu
    
    X-VP-Version: 4.0-beta2
    X-VP-Environment: default

--- wp-content/plugins/wp-floating-menu/inc/backend/includes/wpfm-header.php
+++
@@ -1,7 +0,0 @@
-<?php defined( 'ABSPATH' ) or die( 'No script kiddies please!' ); ?>
-
-<div class="wpfm-setting-header clearfix">
-    <div class="wpfm-header-div">   
-        <p><?php _e( 'WP Floating Menu', 'wp-floating-menu' ); ?></p>
-    </div>   
-</div>    
