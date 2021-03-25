ba21b54a720c2376a6c0367edbef2aacb41ab679
kang <akhsi1@student.monash.edu>
2021-03-25 03:30:42 +0000

    [VP] Deleted plugin 'Floating Action Button'
    VP-Action: plugin/delete/floating-action-button/index.php
    VP-Plugin-Name: Floating Action Button
    
    X-VP-Version: 4.0-beta2
    X-VP-Environment: default

--- wp-content/plugins/floating-action-button/assets/home-conditional-script.js
+++
@@ -1,6 +0,0 @@
-
-jQuery(window).load(function(){
-	jQuery(document).on("click", ".fz-fab-buttons.fz-fab-primary", function(){
-		jQuery(".fz-fab-container").toggleClass("fz-fab-active");
-	});
-});
